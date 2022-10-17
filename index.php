b3875126a569db7297b69ec481f5fd5db82cf2e7
Marine <marine.marez@gmail.com>
2022-10-17 12:30:19 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,94 @@
+<?php
+session_start();
+
+// if(isset($_SESSION['connect'])){
+//     header('location: ../');
+// }
+
+require('src/connection.php');
+
+if(!empty($_POST['pseudo']) && !empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['confirmPassword'])){
+
+// VARIABLES
+    $pseudo = $_POST['pseudo'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $confirmPass = $_POST['confirmPassword'];
+
+    // TEST SI PASSWORD = CONFIRMPASSWORD
+    if($password != $confirmPass){
+        header('location: ./?error=1&pass=1');
+        exit();
+    }
+
+    // TEST SI EMAIL UTILISE
+    $req = $db->prepare('SELECT count(*) AS numberEmail FROM users WHERE email=?');
+    $req->execute(array($email));
+
+    while($emailVerif = $req->fetch()){
+        if($emailVerif['numberEmail'] != 0){
+            header('location: ./?error=1&email=1');
+            exit();
+        }
+    }
+
+    // HASH
+    $secret = sha1($email).time();
+    $secret = sha1($secret).time().time();
+
+    // CRYPTAGE PASSWORD
+    $password = 'aq1'.sha1($password.'1254').'25'; // aq1, sha1, 1254(salt/grain de sel), 25 : méthode d'encryptage sécurisé
+
+    // ENVOI DE LA REQUETE VERS LA BD
+    $req = $db->prepare("INSERT INTO users(pseudo, email, `password`, secret_key) VALUE(?, ?, ?, ?)");
+    $value = $req->execute(array($pseudo, $email, $password, $secret));
+
+    header('location: index.php?success=1');
+    exit();
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>PHP - espace membre</title>
+    <link rel="stylesheet" type="text/css" href="design/default.css">
+</head>
+<body>
+    <h1>Inscription</h1>
+
+    <?php
+    if(!isset($_SESSION['connect'])){ ?>
+
+        <p> Bienvenue sur mon site, pour accéder au conntenu, inscrivez-vous, sinon, <a href="login.php">connectez-vous</a></p>
+        
+        <?php 
+            if(isset($_GET['error'])){
+
+                if(isset($_GET['pass'])){
+                    echo'<p class="error">Mots de passe non identiques</P>';
+                }
+                else if(isset($_GET['email'])){
+                    echo'<p class="error">Cette adresse email existe déjà</p>';
+                }
+            } 
+            else if(isset($_GET['success'])){
+                echo'<p id="success">Votre inscription a bien été prise en compte</p>';
+            }
+        ?>
+        
+        <form action="index.php" method="post">
+        <input type="text" name="pseudo" placeholder="pseudo" required="required">
+        <input type="email" name="email" placeholder="e-mail" required="required">
+        <input type="password" name="password" placeholder="mot de passe" required="required">
+        <input type="password" name="confirmPassword" placeholder="confirmez votre mot de passe" required="required">
+        <button type="submit" >Valider l'inscription</button>
+        </form>
+    <?php } else{ ?> 
+        <p> Bonjour <?= $_SESSION['pseudo'] ?></p>
+
+   <?php } ?>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,75 @@
+<?php
+session_start();
+
+//if(isset($_SESSION['connect'])){
+//    header('location: ./');
+//}
+
+require('src/connection.php');
+
+if(!empty($_POST['email']) && !empty($_POST['password'])){
+
+    //VARIABLES
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $error = 1;
+
+    // CRYPTAGE PASSWORD
+    $password = 'aq1'.sha1($password.'1254').'25';
+
+    echo $password;
+
+    // REQUETE CONNEXION (VÉRIF EMAIL & MDP)
+    $req = $db->prepare('SELECT * FROM users WHERE email = ?');
+    $req->execute(array($email));
+
+     while($user = $req->fetch()){
+        print_r($user);
+        if($password == $user['password']){
+             echo"test";
+
+            header('location: login.php?sucess=1');
+            exit();
+        }
+    }
+    if($error ==1){
+        $error = 0;
+        $_SESSION['connect'] = 1;
+        $_SESSION['pseudo'] = $user['pseudo'];
+        header('location: login.php?error=1');
+        exit();
+    }
+
+}
+
+?> 
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="design/default.css">
+</head>
+<body>
+    <h1>Connexion</h1>
+        <p> Connectez-vous pour accéder au contenu, si vous n'êtes pas inscrit, <a href="index.php">inscrivez-vous</a></p>
+
+        <?php 
+        if(isset($_GET['error'])){
+                echo'<p class="error">Email ou mot de passe non valide</P>';
+            }   
+        else if(isset($_GET['success'])){
+		    echo'<p id="success">Vous êtes maintenant connecté.</p>';
+			}
+        ?>
+
+        <form action="login.php" method="post">
+        <input type="email" name="email" placeholder="e-mail" required="required">
+        <input type="password" name="password" placeholder="mot de passe" required="required">
+        <p class="connection"><label><input class="connection" type="checkbox" name="connect" checked>Connexion automatique</label></P>
+        <button type="submit" >Se connecter</button>
+        </form>
+
+</body>
+</html>
